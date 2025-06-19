46dd46cab12a8daa3b38bbca63649e9902262e1d
volhuuthang <151546166+volhuuthang@users.noreply.github.com>
2025-06-19 07:58:57 +0700

    19/6

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,7 +1,22 @@
+const connection = require('../config/database');
 
 const getHomePage = (req, res) => {
+
+    let users = [];
+
+    connection.query(
+  'select * from Users u;',
+  function (err, results, fields) {
+    users = results;
+    console.log(" >>>results home page = ", results);
+    
+    console.log(" >> check users: ", users)
     res.sedn('Hello World! & nodemon');
+  }
+);
+
 }
+
 
 const getTestPage = (req, res) => {
     res.render('sample.ejs');
--- src/server.js
+++ src/server.js
@@ -3,6 +3,9 @@ const express = require('express')
 const path = require('path')
 const configViewEngine = require('./config/viewEngine');
 const webRoutes = require('./routes/web');
+const mysql = require('mysql2');
+const connection = require('./config/database');
+
 
 const app = express()
 const port = process.env.PORT || 3300;  
@@ -14,6 +17,16 @@ configViewEngine(app);
 
 // route
 app.use('/', webRoutes);
+
+// test database connection
+
+connection.query(
+  'select * from Users u;',
+  function (err, results, fields) {
+    console.log(" >>> results home page = ", results);
+  }
+)
+
 
 app.listen(port, hostname, () => {
   console.log(`Example app listening on port ${port}`)
